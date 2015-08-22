aa3e865a10d86b8c00d384e0e28a0fd12a207c48
mattacraig <ma.craig006@gmail.com>
2015-08-22 10:52:03 -0400

    create post controller actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,51 @@
 class PostsController < ApplicationController 
+  before_action :set_post, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post, notice: 'Great Success!'
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'All Better Now!'
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    
+  end
+
+  def destroy
+    @post.destroy
+  end
+
+  private
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
