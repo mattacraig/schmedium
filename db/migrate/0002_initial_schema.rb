a8dd159dd1de3f3ec408272b4e685bfe62cea960
mattacraig <ma.craig006@gmail.com>
2015-08-22 10:25:56 -0400

    add posts controller with index action for root path

---
+++ db/migrate/0002_initial_schema.rb
@@ -0,0 +1,26 @@
+class InitialSchema < ActiveRecord::Migration
+  def change
+
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+
+    create_table :comments do |t| 
+      t.text :body
+
+      t.timestamps
+    end
+
+    create_table :categories do |t|
+      t.string :name
+    end
+
+    add_reference :posts, :user, index: true        
+    add_reference :posts, :category, index: true
+    add_reference :comments, :user, index: true
+    add_reference :comments, :post, index: true
+  end
+end
